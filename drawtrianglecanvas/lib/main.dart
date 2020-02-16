336b5efa7cb9aa1161207440910725cc749b584e
sri <srilovesflutter@gmail.com>
2020-02-16 10:17:11 +0530

    fixed incorrect code

--- drawtrianglecanvas/lib/main.dart
+++ drawtrianglecanvas/lib/main.dart
@@ -1,95 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var title = "UrlLauncher Demo";
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text('Drawing Traingle with FLutter'),
         ),
-        body: Home(),
+        body: Center(
+          child: 
+            Container(
+              child: CustomPaint(
+                  size: Size(200, 200),
+                  painter: DrawTriangle()
+                ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class Home extends StatelessWidget {
+class DrawTriangle extends CustomPainter {
+
+  Paint _paint;
+
+  DrawTriangle() {
+    _paint = Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.fill;
+  }
+  
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(18.0),
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: _launchurl,
-              child: Text('Goto FlutterCentral'),
-            ),
-            RaisedButton(
-              onPressed: _createEmail,
-              child: Text('Email to FlutterCentral'),
-            ),
-            RaisedButton(
-              onPressed: _makeCall,
-              child: Text('Make a Call'),
-            ),
-            RaisedButton(
-              onPressed: _sendSMS,
-              child: Text('Send an SMS'),
-            ),
-          ],
-        ),
-      ),
-    );
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(size.width/2, 0);
+    path.lineTo(0, size.height);
+    path.lineTo(size.height, size.width);
+    path.close();
+    canvas.drawPath(path, _paint);
   }
 
-  void _launchurl() async{
-    const url = 'https://fluttercentral.com';
-    if(await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }    
-  }
-
-  void _createEmail() async{
-    const emailaddress = 'mailto:admin@fluttercentral.com?subject=Sample Subject&body=This is a Sample email';
-
-    if(await canLaunch(emailaddress)) {
-      await launch(emailaddress);
-    }  else {
-      throw 'Could not Email';
-    }   
-  }
-
-  void _makeCall() async{
-    const phonenumber = "tel:9999999";
-
-    if(await canLaunch(phonenumber)) {
-      await launch(phonenumber);
-    } else {
-      throw 'Could not call';
-    }
-  }
-
-  void _sendSMS() async{
-    const phonenumber = "sms:9999999";
-
-    if(await canLaunch(phonenumber)) {
-      await launch(phonenumber);
-    } else {
-      throw 'Could not SMS';
-    }
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
