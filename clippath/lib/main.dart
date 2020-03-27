6b502e76b488fba3503a985c245d8418f91f025d
sri <srilovesflutter@gmail.com>
2020-03-27 12:00:15 +0530

    Clippath

---
+++ clippath/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State {
+  double padValue = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("BottomWaveClipper"),
+        ),
+        body: Center(
+          child: MyClipPath(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipPath extends StatelessWidget {
+  final Color backgroundColor = Colors.red;
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: BottomWaveClipper(),
+      child: Container(
+        color: backgroundColor,
+        width: 300,
+        height: 200,
+      ),
+    );
+  }
+}
+
+class BottomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height - 40);
+    path.quadraticBezierTo(
+        size.width / 4, size.height - 80, size.width / 2, size.height - 40);
+    path.quadraticBezierTo(size.width - (size.width / 4), size.height,
+        size.width, size.height - 40);
+    path.lineTo(size.width, 0.0);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
